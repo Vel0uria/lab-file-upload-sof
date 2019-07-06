c9231d242d246eb9a9a92d327296b6d7fe6d41da
Francisco <franperceb@gmail.com>
2019-07-06 12:44:24 -0500

    done

--- starter-code/models/Post.js
+++ starter-code/models/Post.js
@@ -1,20 +1,31 @@
 //const { model, Schema } = require("mongoose");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+
+const commentSchema = Schema({
+  content: String,
+  commentId: Schema.ObjectId,
+  imgPath: String,
+  imgName: String
+});
+
 const postSchema = Schema(
   {
     content: String,
     creatorId: {
-      type: Schema.Types.ObjectId
+      type: Schema.ObjectId
     },
     picPath: String,
-    picName: String
+    picName: String,
+    comments: [commentSchema]
   },
   {
     timestamps: true,
     versionKey: false
   }
 );
+
 
 //module.exports = model("Post", postSchema);
 const Post = mongoose.model("Post", postSchema);
